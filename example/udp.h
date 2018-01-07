2656c27b53aafbb2c57c0a4cf49298aef2213d8b
Rolf Madsen <rolfrm@gmail.com>
2018-01-07 23:27:58 +0100

    Removed some unused files.

--- example/udp.h
+++
@@ -1,3 +0,0 @@
-struct sockaddr_storage udp_get_addr(char * remote_address, int port);
-int udp_connect(struct sockaddr_storage * local, struct sockaddr_storage * remote, bool socket_reuse);
-int udp_open(struct sockaddr_storage * local);
